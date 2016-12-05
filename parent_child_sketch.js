441b6c68d1d49e2d052b2587a73d2f7665349bee
Frank Giraffe <fgiraffe@gmail.com>
2016-12-05 14:27:09 -0800

    In the middle of the 7.12 tutorial on parent-child attachments, etc.

---
+++ parent_child_sketch.js
@@ -0,0 +1,11 @@
+
+var gCanvas;
+function setup() {
+    gCanvas = createCanvas(300,300);
+    gCanvas.parent('canvas_para');
+}
+
+function draw() {
+background(127);
+ellipse(width/2, height/2, 100, 100);
+}
