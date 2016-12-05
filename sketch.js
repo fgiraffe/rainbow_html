9025057add9f568a5cc18bf980fe6536198fd0a7
Frank Giraffe <fgiraffe@gmail.com>
2016-12-04 17:54:44 -0800

    html vid 7.6

--- sketch.js
+++ sketch.js
@@ -5,8 +5,13 @@ var canv;
 var myH1;
 var bgColor;
 var myNewButt;
+var sliderText;
+var mySlide;
+var myInput;
 
-function changeStyle () {
+var defHeaderText = "Text from js!"
+
+function changeStyle() {
     txt.style("color", "black");
     padding += 10;
     padStr = padding + "px";
@@ -23,15 +28,27 @@ function changeColor() {
     bgColor = color(random(255), random(255), random(255));
 }
 
+function overpara() {
+    myH1.html("Spooky!");
+}
+
+function outPara() {
+    myH1.html(defHeaderText);
+}
+
+function updateInputText() {
+    myH1.html(myInput.value());
+}
+
 function setup() {
 
     bgColor = color(200);
 
     canv = createCanvas(200, 200);
-    canv.position(200,300);
+    canv.position(200, 300);
 
-    myH1 = createElement("h1", "Waiting. (from js)");
-    myH1.position(400,100);
+    myH1 = createElement("h1", defHeaderText);
+    myH1.position(400, 100);
 
     createP("Fave color is blue!(from js)");
     createP("Fave color is blue! (from js)");
@@ -44,15 +61,27 @@ function setup() {
 
     myNewButt = createButton("Change Color");
     myNewButt.mousePressed(changeColor);
+    createP(""); // empty para to separate button
+    mySlide = createSlider(10, 100, 86);
+
+    createP(""); // empty para to separate text field
+    myInput = createInput("Type your name!");
+    myInput.changed(updateInputText);
+
+    myH1.mouseOver(overpara);
+    myH1.mouseOut(outPara);
+
 }
 
 function redRect() {
     fill(255, 170, 0);
-    rect(100, rx, 50, 50);
+    rect(100, mySlide.value(), 50, 50);
     // rx = (rx + 10) % 200;
 }
 
 function draw() {
     background(bgColor);
-    redRect()
+    redRect();
+    text(myInput.value(), 20, 20);
+
 }
