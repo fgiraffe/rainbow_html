7e8a0e4fb85639196c4b84fa683d41d89b707e49
Frank Giraffe <fgiraffe@gmail.com>
2016-12-20 11:20:20 -0500

    handle running out of linked words better. show what the app does in the comments

--- w03/wordnik88_script.js
+++ w03/wordnik88_script.js
@@ -3,9 +3,13 @@ var PASSCODE = "17c2d1d1742e7b2da2a4a08bbf8063113ecf81f894c68514d";
 // example for "ubiquity"
 // http://api.wordnik.com:80/v4/word.json/ubiquity/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
 
-
+// present user with a word.
+// every time they click on it, find a related word using wordnik.
+// show the relation, and add the word to display.
+// when we run out of related words, show GAME OVER
 
 var wordLink;
+var chainPara;
 
 function setup() {
     noCanvas();
@@ -14,6 +18,9 @@ function setup() {
 
     wordLink = createA("#", word); // empty link for our word
     wordLink.mousePressed(askWordNik);
+
+    chainPara = createP(word);
+
 
 }
 
@@ -27,18 +34,45 @@ function askWordNik() {
     var apiKeyString = "&api_key=" + PASSCODE;
 
     var request = urlHeader + wordToLookup + urlTrailer_RelatedWords + apiKeyString;
-    var respns = loadJSON(request, gotWNData);
+    loadJSON(request, gotWNData);
+}
+
+function endGame() {
+    wordLink.html("GAME OVER");
+    wordLink.mousePressed(doNothing);
+}
+
+function doNothing() {
+
 }
 
 function gotWNData(wnData) {
 
-    println(wnData);
-    var newWordIndex = floor(random(0, wnData.length));
+    if (!wnData || (wnData.length <= 0)) {
+        // the chain of related words is NOT infinite.
+        // We can eventually run out of words that relate.
+        // so do.. something?
+        endGame();
+    } else {
+        var countOfRelatedWordTypes = wnData.length;
+        var randTypeOfRelatedWord = floor(random(0, countOfRelatedWordTypes));
 
-    println(newWordIndex);
-    wordLink.html(wnData[newWordIndex].words[0]);
-}
+        var countOfRelatedWords = wnData[randTypeOfRelatedWord].words.length;
+        var randomWordIndex = floor(random(0, countOfRelatedWords));
 
-function draw() {
+        if (countOfRelatedWords > 0) {
+            var newWord = wnData[randTypeOfRelatedWord].words[randomWordIndex];
+
+            wordLink.html(newWord);
+
+            var oldchain = chainPara.html();
+            var newStr = newWord + " <- (" +
+                    wnData[randTypeOfRelatedWord].relationshipType + ")" + oldchain;
+            chainPara.html(newStr);
+        } else {
+            endGame();
+        }
+
+    }
 
 }
